1a3a5c3f5a776df031455d81e9a5449da66d5846
Fran <you@example.com>
2025-06-27 11:28:16 -0300

    reordenamiento del repositorio, falta comprobar funcionalidad

--- EntregaFinal.sql
+++ EntregaFinal.sql
@@ -248,7 +248,7 @@ create view mejor_vendedor as select usuario.nombre from usuario join publicacio
 
 #------------------------------Triggers------------------------------#
 
-#1)------------------------------------------------------------------------------------------------
+#1)
 delimiter //
 create trigger borrarPreguntas before delete on publicacion for each row 
 begin
@@ -270,7 +270,6 @@ begin
 		
 end //
 delimiter ;
-
 
 #3)------------------------------------------------------------------------------------------------
 delimiter //
@@ -318,9 +317,76 @@ delimiter ;
 #1)
 create index indexFKproducts on publicacion(producto_idProducto);
 create index FindNames on producto(nombreProducto);
+
 #2)------------------------------------------------------------------------------------------------
 create unique index dontRepeatEmail on usuario(email);
+
 #3)------------------------------------------------------------------------------------------------
 create index statusAPF on publicacion(estado);
 
+#------------------------------Transacciones------------------------------#
 
+#1)
+delimiter //
+create procedure crearPublicacion (in idPubli int, in cuantoDuele float, in nivelPubli varchar(45), in estado varchar(45), in tipo text, in idProdu int, in idCat int, in DNI int, in fecha datetime, in titulito text)
+begin 
+start transaction;
+		insert publicacion values(idPubli,cuantoDuele,nivelPuli,estado,tipo,idProdu,idCat,DNI,fecha,titulito);
+ if not exists (select publicacion.id from publicacion where id = publicacion.idPubli) then
+        commit;
+        select "Publicación creada con éxito." as mensaje;
+    else
+        rollback;
+        signal sqlstate '45000' set message_text = "La publicación ya existe.";
+    end if;
+end;
+end //
+delimiter ;
+
+delimiter //
+create procedure crearSubasta(in ofert int, in idPubli int, in tope datetime, in minimo double)
+begin 
+start transaction;
+insert into subasta values(ofert, idPubli, tope, minimo);
+if not exists (select publicacion_id from venta_directa where publicacion_id=idPubli) then 
+commit;
+        select "subasta creada con éxito." as mensaje;
+    else
+        rollback;
+        signal sqlstate '45000' set message_text = "La publicación es de venta directa";
+end if;
+end //
+delimiter ;
+
+#2)------------------------------------------------------------------------------------------------
+delimiter //
+create procedure actualizarReputacionUsuarios()
+begin 
+declare hayFilas int default 1;
+declare usuario int default 0;
+declare promedio1 int default 0;
+declare promedio2 int default 0;
+declare promedio3 int default 0;
+
+
+declare cursorCalificaciones cursor for select usuario_DNI, avg(calificacion_compra), avg(calificacion_vendedor) from publicacion join compra on publicacion.id = publicacion_id group by usuario_DNI; 
+declare continue handler for not found set hayFilas = 0;
+open cursorCalificaciones;
+loopsito:loop
+	fetch cursorCalificaciones into usuario, promedio1, promedio2;
+    if hayFilas = 0 then 
+		leave loopsito;
+	end if;
+    start transaction;
+    set promedio3 =(promedio1 + promedio2) / 2;
+    update usuario set reputacion = (promedio1 + promedio2) / 2 where DNI = usuario;
+    if promedio3>=1 and promedio3<=5 then
+            commit;
+	else 
+		rollback;
+        signal sqlstate '45000' set message_text = "ERROR resultado de reputacion invalido.";
+    end if;
+    end loop;
+    close cursorCalificaciones;
+    end //
+    delimiter ;
